4056daaefb7daa8c2279cf50d22ed98b06e16c3c
Tim <tmetcalfe89@gmail.com>
2019-12-21 09:16:44 -0800

    Moved modinfo to its own class

--- src/main/java/com/legobmw99/oldobsidian/OldObsidian.java
+++ src/main/java/com/legobmw99/oldobsidian/OldObsidian.java
@@ -16,12 +16,10 @@ import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import us.timinc.jsonedoldobsidian.ModInfo;
 
-@Mod(modid = OldObsidian.MODID, version = OldObsidian.VERSION)
+@Mod(modid = ModInfo.MODID, version = ModInfo.VERSION)
 public class OldObsidian {
-	public static final String MODID = "oldobsidian";
-	public static final String VERSION = "1.1";
-
 	@EventHandler
 	public void preInit(FMLPreInitializationEvent event) {
 		MinecraftForge.EVENT_BUS.register(this);
---
+++ src/main/java/us/timinc/jsonedoldobsidian/ModInfo.java
@@ -0,0 +1,6 @@
+package us.timinc.jsonedoldobsidian;
+
+public class ModInfo {
+	public static final String MODID = "jsonedoldobsidian";
+	public static final String VERSION = "0.1";
+}
